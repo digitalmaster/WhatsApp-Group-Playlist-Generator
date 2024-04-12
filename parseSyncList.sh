a285c42faf7d0b126ee02243b598352ccbd5e43e
Jose Browne <josebrowne@gmail.com>
2024-04-12 18:19:35 -0400

    Only add new tracks

--- parseSyncList.sh
+++ parseSyncList.sh
@@ -38,9 +38,9 @@ authorize(){
     # Get the access token
     ACCESS_TOKEN=$(echo $ACCESS_TOKEN_RESPONSE | jq -r '.access_token')
     ACCESS_ERROR=$(echo $ACCESS_TOKEN_RESPONSE | jq -r '.error')
-    echo "Access error: $ACCESS_ERROR"
 
     if [ -z "$ACCESS_TOKEN" ] || [ "$ACCESS_ERROR" != "null" ]; then
+        echo "Access error: $ACCESS_ERROR"
         echo "🚫 Failed to get access token 😢"
         exit 1
     else
@@ -48,11 +48,24 @@ authorize(){
     fi
 }
 
+removeTracksAlreadyInPlaylist(){
+    local TRACK_LIST=$1
+
+    # Convert existing track IDs into a grep-friendly pattern
+    local PATTERN=$(echo "$EXISTING_TRACKS" | sed 's/ /\\|/g')
+
+    # Filter TRACK_LIST by removing entries matching the pattern
+    local FILTERED_TRACK_LIST=$(echo "$TRACK_LIST" | grep -v "$PATTERN")
+
+    echo "$FILTERED_TRACK_LIST"
+}
+
 TRACK_LIST=''
 loadList(){
+    local TRACK_DUPLICATES=''
     # Parse the WhatsApp group export txt file and extract the spotify track links
     TRACK_LIST=$(grep -o 'https://open.spotify.com/track/[a-zA-Z0-9]*' < _chat.txt)
-    local TRACK_DUPLICATES=$(echo "$TRACK_LIST" | sort | uniq -d)
+    TRACK_DUPLICATES=$(echo "$TRACK_LIST" | sort | uniq -d)
 
     # Print duplicates
     if [ -n "$TRACK_DUPLICATES" ]; then
@@ -65,6 +78,70 @@ loadList(){
 
     # Print number of tracks found
     echo "Found $(echo "$TRACK_LIST" | wc -l | tr -d '[:space:]') tracks"
+
+    # Remove tracks already in the playlist
+    TRACK_LIST_COUNT_BEFORE=$(echo "$TRACK_LIST" | wc -l | tr -d '[:space:]');
+    TRACK_LIST=$(removeTracksAlreadyInPlaylist "$TRACK_LIST")
+
+    if [ -z "$TRACK_LIST" ]; then
+        echo "✅ All $TRACK_LIST_COUNT_BEFORE already added. No new tracks to add"
+        exit 0
+    fi
+
+    TRACK_LIST_COUNT_AFTER=$(echo "$TRACK_LIST" | wc -l | tr -d '[:space:]');
+    echo "✨ Found $TRACK_LIST_COUNT_AFTER new tracks to add 🎵"
+    echo "$TRACK_LIST"
+}
+
+fetchTracks(){
+    local PLAYLIST_TRACKS_RESPONSE=''
+
+    PLAYLIST_TRACKS_RESPONSE=$(curl -s -X GET "$1" \
+        -H "Authorization: Bearer $ACCESS_TOKEN")
+
+    echo "$PLAYLIST_TRACKS_RESPONSE"
+}
+
+
+fetchExistingTracks(){
+    echo "Fetching existing tracks from the playlist..."
+
+    local FETCH_RESPONSE=''
+    local NEXT_URL=''
+    local EXISTING_TRACKS_COUNT=''
+
+    FETCH_RESPONSE=$(fetchTracks "https://api.spotify.com/v1/playlists/$SPOTIFY_PLAYLIST_ID/tracks?limit=100&offset=0")
+
+    FETCH_ERROR=$(echo "$FETCH_RESPONSE" | jq -r '.error')
+
+    if [ "$FETCH_ERROR" != "null" ]; then
+        echo "Fetch error: $FETCH_ERROR"
+        echo "Response: $FETCH_RESPONSE"
+        echo "🚫 Failed to fetch existing tracks 😢"
+        exit 1
+    fi
+    EXISTING_TRACKS=$(echo "$FETCH_RESPONSE" | jq -r '.items[].track.id');
+    NEXT_URL=$(echo "$FETCH_RESPONSE" | jq -r '.next')
+
+    while [ "$NEXT_URL" != "null" ]; do
+        echo "Fetching next page of tracks: $NEXT_URL"
+        FETCH_RESPONSE=$(fetchTracks "$NEXT_URL")
+        FETCH_ERROR=$(echo "$FETCH_RESPONSE" | jq -r '.error')
+
+        if [ "$FETCH_ERROR" != "null" ]; then
+            echo "Fetch error: $FETCH_ERROR"
+            echo "Response: $FETCH_RESPONSE"
+            echo "🚫 Failed to fetch existing tracks 😢"
+            exit 1
+        fi
+
+        EXISTING_TRACKS+=$'\n'
+        EXISTING_TRACKS+="$(echo "$FETCH_RESPONSE" | jq -r '.items[].track.id')"
+        NEXT_URL=$(echo "$FETCH_RESPONSE" | jq -r '.next')
+    done
+
+    EXISTING_TRACKS_COUNT=$(echo "$EXISTING_TRACKS" | wc -l | tr -d '[:space:]')
+    echo "✅ Successfully fetched $EXISTING_TRACKS_COUNT existing tracks 🎵"
 }
 
 convertToSpotifyURIs(){
@@ -85,21 +162,33 @@ createTrackListGroups(){
 }
 
 updatePlaylist(){
+    echo "Updating playlist..."
     for group_list_string in "${TRACK_LIST_GROUPS[@]}"; do
         #split the string by space and count the number of items
         group_list_length=$(echo "$group_list_string" | wc -w)
         echo "Adding: $group_list_length tracks to playlist..."
 
-        echo "Adding tracks to playlist: $group_list_string"
         array_string=$(convertSpotifyURIsToArrayString "$group_list_string")
-        curl -X POST "https://api.spotify.com/v1/playlists/$SPOTIFY_PLAYLIST_ID/tracks" \
+        response=$(curl -s -X POST "https://api.spotify.com/v1/playlists/$SPOTIFY_PLAYLIST_ID/tracks" \
             -H "Authorization: Bearer $ACCESS_TOKEN" \
             -H "Content-Type: application/json" \
-            -d "{\"uris\": $array_string}"
+            -d "{\"uris\": $array_string}")
+        response_error=$(echo "$response" | jq -r '.error')
+
+        if [ "$response_error" != "null" ]; then
+            echo "Response error: $response_error"
+            echo "Response: $response"
+            echo "🚫 Failed to update playlist 😢"
+            exit 1
+        fi
     done
+
+    echo "✅ Successfully updated playlist 🎵"
 }
 
 authorize
+EXISTING_TRACKS=''
+fetchExistingTracks
 loadList
 TRACK_LIST=$(convertToSpotifyURIs)
 TRACK_LIST_GROUPS=()
